07517d90d3c511c4e3a61b7bb72ae64d84b59738
DanMizen <90823751+DanMizen@users.noreply.github.com>
2021-09-16 22:07:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import random
 from discord.ext import commands
 
 
-
 client = commands.Bot(command_prefix = '$')
 
 @client.event
@@ -19,9 +18,6 @@ async def on_member_remove(member):
     print(f'{member} has left the server!')
 
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'pong! {round(client.latency * 1000)}ms')
 
 @client.event
 async def on_message(message):
@@ -30,5 +26,5 @@ async def on_message(message):
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
 
-client.run('ODg3NzY4OTIzMDc5MDc3OTM5.YUI9PQ.7JzLgHoPCi7U33Q5fO5FCbbvxLk')
+client.run('your token')
 
